1fb7dd77ac8ce47df3fd7bf8b4ab6c6bb924543e
morski.przemek@gmail.com <6xQjRAQ9icTX>
2020-11-03 19:59:32 +0300

    firebase config added to prod env

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,3 +1,13 @@
 export const environment = {
-  production: true
+  production: true,
+  firebaseConfig: {
+    apiKey: 'AIzaSyCDV1koraLa79AljZEmV6eQQhcpKL6Pmw8',
+    authDomain: 'usersgrid.firebaseapp.com',
+    databaseURL: 'https://usersgrid.firebaseio.com',
+    projectId: 'usersgrid',
+    storageBucket: 'usersgrid.appspot.com',
+    messagingSenderId: '825942875125',
+    appId: '1:825942875125:web:63929a65eed00c37c71ebc',
+    measurementId: 'G-BV814GWMP8'
+  }
 };
